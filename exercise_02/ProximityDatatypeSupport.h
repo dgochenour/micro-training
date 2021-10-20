9b6da0d866d8932dbb070915f864e86f3e2969bc
Don Gochenour <don@rti.com>
2021-10-20 16:46:11 -0400

    change default send period to 500ms, uncomment deadline QoS

---
+++ exercise_02/ProximityDatatypeSupport.h
@@ -0,0 +1,55 @@
+/*
+WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
+
+This file was generated from ProximityDatatype.idl using "rtiddsgen".
+The rtiddsgen tool is part of the RTI Data Distribution Service distribution.
+For more information, type 'rtiddsgen -help' at a command shell
+or consult the RTI Data Distribution Service manual.
+*/
+
+#ifndef ProximityDatatypeSupport_1543506996_h
+#define ProximityDatatypeSupport_1543506996_h
+
+#include <stdlib.h>
+
+/* Uses */
+#include "ProximityDatatype.h"
+/* Requires */
+#include "ProximityDatatypePlugin.h"
+
+#if (defined(RTI_WIN32) || defined(RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+
+/* If we're building on Windows, explicitly import the superclasses of
+* the types declared below.
+*/
+class __declspec(dllimport) DDSDataWriter;
+class __declspec(dllimport) DDSDataWriter_impl;
+class __declspec(dllimport) DDSDataReader;
+class __declspec(dllimport) DDSDataReader_impl;
+
+#endif
+
+/* ========================================================================== */
+/**
+Uses:     T
+Defines:  TTypeSupport, TDataWriter, TDataReader*/
+
+#if (defined(RTI_WIN32) || defined(RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, start exporting symbols. */
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport __declspec(dllexport)
+#endif
+
+DDS_TYPESUPPORT_CPP(ProximityDataTypeSupport, ProximityData);
+DDS_DATAWRITER_CPP(ProximityDataDataWriter, ProximityData);
+
+DDS_DATAREADER_CPP(ProximityDataDataReader, ProximityData);
+
+#if (defined(RTI_WIN32) || defined(RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, stop exporting symbols. */
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport
+#endif
+
+#endif  /* ProximityDatatypeSupport_1543506996_h */
+
