11bba0abf37917e03510d0cce8a77bbc3c631856
Don Gochenour <don@rti.com>
2021-10-27 15:01:16 -0400

    initial working BrakeCommand publication

---
+++ exercise_04/brake.cxx
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main() {
+
+    // create the DomainParticipant
+    // create Subscriber
+    // create Topic
+    // create DataReader
+        // handle incoming data
+    return EXIT_SUCCESS;
+}
